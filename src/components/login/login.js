b318bc34dee8633dd00e28096b7eb3a8d648718e
Yauheni Rusinchuk <yauheni_rusinchuk@senla.eu>
2020-04-15 23:19:12 +0300

    design login form

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 import './login.css'
 
+
 const Login = () => {
   return (
     <div className='login'>
-      <div className='login__form'>
-        <p className='login__form__title'>LOGIN PLEASE</p>
-      </div>
+      <p className='login__form__title'>LOGIN</p>
+      <form className='login__form'>
+        <input type='email' placeholder='Email ...' />
+        <input type='password' placeholder="Password ..." />
+        <button>LOGIN</button> 
+      </form>
     </div>
   )
 } 
