186543d9a457466f226f5e483eceedf09b2e2a3f
Yauheni Rusinchuk <yauheni_rusinchuk@senla.eu>
2020-04-16 00:37:30 +0300

    added home page design

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,7 +5,20 @@ import './home.css'
 const Home = () => {
   return (
     <div className='home'>
-      HOME
+      <div className='home__navigator'>
+        <div className='home__navigator__title'>
+          <p>CHECK YOU MONEY</p>
+          <p>ALL BUDGET <span>$325</span></p>
+        </div>
+        <div className='home__navigator__btns'>
+          <button className='home__navigator__btn__new'>NEW EXPENSES</button>
+          <button className='home__navigator__btn__logout'>LOGOUT</button>
+        </div>
+      </div>
+
+      <div className='home__wrapper'>
+        MAIN
+      </div>
     </div>
   )
 }
