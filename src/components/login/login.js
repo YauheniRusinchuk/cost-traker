46f8b06acfeda07adcce59c94bbe7f496864e4a9
YauheniRusinchuk <ruevgal@gmail.com>
2020-04-15 22:56:00 +0300

    add configurate pages

---
+++ src/components/login/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './login.css'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <div className='login__form'>
+        <p className='login__form__title'>LOGIN PLEASE</p>
+      </div>
+    </div>
+  )
+} 
+
+export default Login;
